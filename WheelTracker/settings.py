ddd26fac2ac244f031783f1d2bd0c7db40ace545
Shaisab Mistry <145130336+shaisabm@users.noreply.github.com>
2025-10-31 21:52:41 -0400

    Update settings.py: remove hardcoded SECRET_KEY for improved security

--- WheelTracker/settings.py
+++ WheelTracker/settings.py
@@ -26,7 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-9j+vrh-h_sl+ru-62g1wme&t1jzdwa9#+*qul86*+92o7m!*v4')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'

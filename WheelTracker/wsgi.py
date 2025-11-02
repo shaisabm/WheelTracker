f871caadfe87fd7998f41b3fc480b256edd05601
Shaisab Mistry <145130336+shaisabm@users.noreply.github.com>
2025-11-02 15:24:36 -0500

    Update Vercel configuration for Python deployment and add serverless function handler

--- WheelTracker/wsgi.py
+++ WheelTracker/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WheelTracker.settings")
 
 application = get_wsgi_application()
+
+# Vercel serverless function handler
+app = application
